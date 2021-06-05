6ccff5a038a0d1cef4e4f4c0151868b604f9eb4a
Anil <billaanil3@gmail.com>
2021-06-05 20:58:15 +0530

    Added models to Django Admin Panel

--- visitor/admin.py
+++ visitor/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from visitor.models import Country
+from visitor.models import State
+from .models import Location
+from .models import User
+from .models import Organization
+from .models import Department
+from .models import Visitor
+from .models import Employee
+from .models import VisitDetails
+
+admin.site.register(Country)
+admin.site.register(State)
